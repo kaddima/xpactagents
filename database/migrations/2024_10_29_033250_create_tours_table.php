f498523a6f45b5d92603e3f5ff32dd948a2ceec9
kadima <williamsvgw@gmail.com>
2024-10-29 02:40:24 +0000

    migration schema for tours table

---
+++ database/migrations/2024_10_29_033250_create_tours_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateToursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tours', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('email')->unique();
+            $table->string("phone")->nullable();
+            $table->timestamp("date");
+            $table->text("notes");
+            $table->string("best_contact");
+            $table->uuid("property_id");
+            $table->tinyInteger("resolved")->default(0);
+            $table->uuid("agent_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tours');
+    }
+}
